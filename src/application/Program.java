f95a586e8f694f7672384f3118698981d9d7e36d
stehames <stehh.amess@gmail.com>
2021-05-10 10:19:51 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
-/**
- *
- * @author smc_s
- */
+import boardgame.Position;
+
 public class Program {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hello!");
+       
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
     
 }
